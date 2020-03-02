5a5e17f0773de74dd5b22ca1517341ab795b26fd
tylerpinney88 <56723285+tylerpinney88@users.noreply.github.com>
2020-03-02 12:37:53 +0000

    Game Updated

---
+++ Controller Game/Assets/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+    public Transform Camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate() 
+    {
+        Camera.position = new Vector3(Player.position.x, Camera.position.y, Camera.position.z);
+    }
+}
--- Controller Game/Assets/PlayerController.cs
+++ Controller Game/Assets/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float JumpChanceCountdown;
+    public int ChanceToJump;
+    public Rigidbody2D PlayerRb;
+    public int Speed;
+    public float JumpChanceTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        JumpChanceCountdown = JumpChanceTimer;
+        ChanceToJump = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(1, 1, 0) * Time.deltaTime;
+        transform.position = transform.position + new Vector3(Speed, 0, 0) * Time.deltaTime;
+        JumpChance();
     }
+
+    void JumpChance()
+    {
+        JumpChanceCountdown = JumpChanceCountdown -= Time.deltaTime;
+
+        if (JumpChanceCountdown <= 0f)
+        {
+            JumpChanceCountdown = JumpChanceTimer;
+            ChanceToJump = Random.Range(0, 3);
+        }
+
+        if (ChanceToJump == 0)
+        {
+            Jump();
+            ChanceToJump = 3;
+        }
+
+        else
+        {
+            return;
+        }
+    }
+    void Jump()
+    {
+        PlayerRb.AddForce(new Vector2(1, 10), ForceMode2D.Impulse);
+    }
+
 }
